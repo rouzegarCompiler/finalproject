0485c238270b5f6863aec7b9c3b0f31afaccb986
smr <mohammad>
2022-08-31 12:51:17 +0430

    Initilize user model and add user blueprint to main app

--- project/__init__.py
+++ project/__init__.py
@@ -1,7 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from .config import Development,Production
+
 
 app = Flask(__name__)
 app.config.from_object(Development)
+
+# initilize dependencies
+db = SQLAlchemy(app)
+
+# blueprints
+from mod_user import user
+
+# register blueprints
+app.register_blueprint(user)
 
 from . import views
---
+++ project/mod_user/models.py
@@ -0,0 +1,10 @@
+from project import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer(), primary_key = True)
+    fname = db.Column(db.String(length = 24), nullable = False)
+    lname = db.Column(db.String(length = 24), nullable = False)
+    email = db.Column(db.String(length = 160), nullable = False , unique = True)
+    gender = db.Column(db.Integer(), nullable = False)
+    role = db.Integer(db.Integer(), nullable = False, default = 0)
