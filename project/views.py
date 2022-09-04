190f2e834b61d447b5e3888477f96dc7c4e1fb07
smr <mohammad>
2022-09-04 06:23:16 +0430

    Some changes added to project

--- project/views.py
+++ project/views.py
@@ -1,10 +1,6 @@
-from urllib.parse import urldefrag
-from flask import render_template, redirect, url_for
-
-from flask_login import login_required, current_user
+from flask import redirect, url_for
 
 from . import app
-
 
 @app.route('/')
 def index():
